f7fd6e9cc57cfd54656d5896d5c17b757430e817
hitheswar <dama.hitheswar@aline-consulting.com>
2020-01-31 17:48:46 +0530

    cassandra connector

---
+++ build.sbt
@@ -0,0 +1,23 @@
+name := "Spark-Connectors"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+lazy val sparkVersion = "2.4.4"
+
+resolvers ++= Seq(
+  "apache-snapshots" at "http://repository.apache.org/snapshots/"
+)
+
+resolvers ++= Seq(
+  "apache-snapshots" at "https://repo1.maven.org/maven2/"
+)
+
+//spark
+libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion
+libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion
+
+//cassandra dependeincies
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "2.4.1"
+libraryDependencies += "com.twitter" % "jsr166e" % "1.1.0"
--- src/main/scala/com/dama/spark/nosqlDB/Cassandra.scala
+++ src/main/scala/com/dama/spark/nosqlDB/Cassandra.scala
@@ -1,5 +1,49 @@
 package com.dama.spark.nosqlDB
 
-object Cassandra {
+import org.apache.spark.SparkConf
+import org.apache.spark.sql._
+import com.dama.spark.util.Constants._
+import org.apache.log4j.{Level, Logger}
+
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.{CassandraConnector, _}
+import com.datastax.spark.connector.rdd.CassandraTableScanRDD
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.cassandra._
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
+object Cassandra extends App{
+
+  println("hello world")
+
+  lazy val sparkConf = new SparkConf()
+    .set("spark.cassandra.connection.host", cassandra_host)
+    .set("spark.cassandra.auth.username", cassandra_user)
+    .set("spark.cassandra.auth.password", cassandra_pass)
+    .setAppName("Cassandra Test")
+    .setMaster("local")
+    .set("spark.cores.max", "2")
+  val sparkSession = SparkSession
+    .builder()
+    .config(sparkConf)
+    .getOrCreate()
+
+  val rootLogger = Logger.getRootLogger()
+  rootLogger.setLevel(Level.ERROR)
+  rootLogger.setLevel(Level.INFO)
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
+
+  var cas_df = sparkSession.read.format("org.apache.spark.sql.cassandra")
+    .options(Map("table" -> "customers", "keyspace" -> "cloudcdc")).load()
+
+  cas_df.show()
+  println(cas_df.count())
+
+  cas_df.write.cassandraFormat("customers_new", "cloudcdc")
+    .mode(SaveMode.Overwrite).option("confirm.truncate", "true").save()
 
 }
+
+
