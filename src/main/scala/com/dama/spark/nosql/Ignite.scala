0897dcc3144261bce5e4be0ad54eadec2736cf66
hitheswar <dama.hitheswar@aline-consulting.com>
2020-02-20 17:31:32 +0530

    developed ignite thick client

--- src/main/scala/com/dama/spark/nosql/Ignite.scala
+++ src/main/scala/com/dama/spark/nosql/Ignite.scala
@@ -1,21 +1,30 @@
 package com.dama.spark.nosql
 
 import com.dama.spark.core.SparkApp
-import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.{DataFrame, SaveMode}
 import java.util.Properties
 
 import com.dama.spark.services.IgniteService
-import com.dama.spark.util.Constants.{ignite_pass, ignite_url, ignite_usr}
+import com.dama.spark.util.Constants.{config, ignite_pass, ignite_url, ignite_usr}
 
 object Ignite extends App with SparkApp {
 
-  val connection_url = ignite_url+ ";user=" + ignite_usr + ";password=" + ignite_pass
+  //ThinClient
+  /*val connection_url = ignite_url+ ";user=" + ignite_usr + ";password=" + ignite_pass
   var df = IgniteService.readThroughIgniteThinClient(spark,connection_url,"SITE")
   df.show(10)
   val count = df.count()
   println("count  "+count)
+  IgniteService.writeThroughIgniteThinClient(df,"SITES",connection_url)*/
 
-  IgniteService.writeThroughIgniteThinClient(df,"SITES",connection_url)
+  //ThickClient
+  var dataFrame:DataFrame = IgniteService.readThroughIgniteThickClient(spark,"SITE",config)
+  dataFrame.show()
+  dataFrame.createOrReplaceTempView("site")
+  dataFrame.printSchema()
+  println(dataFrame.count())
+  spark.sql("select * from site order by asc").show(5)
 
+  IgniteService.writeThroughIgniteThickClient(spark,dataFrame,"sites",config,SaveMode.Append)
 
 }
--- src/main/scala/com/dama/spark/services/IgniteService.scala
+++ src/main/scala/com/dama/spark/services/IgniteService.scala
@@ -2,10 +2,27 @@ package com.dama.spark.services
 
 import java.util.Properties
 
+import org.apache.ignite.spark.IgniteDataFrameSettings.{FORMAT_IGNITE, OPTION_CONFIG_FILE, OPTION_TABLE,OPTION_STREAMER_ALLOW_OVERWRITE}
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 
 object IgniteService {
+  def readThroughIgniteThickClient(spark: SparkSession, tableName: String, config: String): DataFrame = {
+    spark.read.format(FORMAT_IGNITE).option(OPTION_CONFIG_FILE, config).option(OPTION_TABLE, tableName).load().cache()
+  }
+  def writeThroughIgniteThickClient(spark:SparkSession,finalDF:DataFrame,tableName:String,config:String,saveMode: SaveMode): Unit ={
+
+    if (saveMode == SaveMode.Append) {
+      finalDF.write.format(FORMAT_IGNITE).option(OPTION_CONFIG_FILE, config).option(OPTION_TABLE, tableName).option(OPTION_STREAMER_ALLOW_OVERWRITE, true).mode(saveMode).save()
+    }
+    else if(saveMode == SaveMode.Overwrite){
+
+    }
+    else{
+
+    }
+  }
+
   def readThroughIgniteThinClient(spark: SparkSession, connection_url: String,tableName:String): DataFrame={
     val connectionProperties = new Properties()
     connectionProperties.put("url", connection_url)
